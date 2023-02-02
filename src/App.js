055a677f38ab910873d76c6474b614b6c59e3cc5
Vaghela Sagar <sagar19vaghela.sv@gmail.com>
2023-02-02 15:05:15 +0530

    change method of download

--- src/App.js
+++ src/App.js
@@ -3,22 +3,6 @@ import { items } from "./response";
 import FileSaver from "file-saver";
 
 function App() {
-  const getTableData = () => {
-    const excelData = [];
-    items.forEach((data, i) => {
-      const obj = {};
-      if (i > 0) {
-        items[0].forEach((item, i) => {
-          obj[item] = data[i];
-        });
-        if (Object.keys(obj).length !== 0) {
-          excelData.push(obj);
-        }
-      }
-    });
-    return excelData;
-  };
-
   const saveAsExcelFile = (buffer, fileName, extension) => {
     let EXCEL_TYPE =
       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
@@ -30,8 +14,7 @@ function App() {
 
   const handleClick = () => {
     import("xlsx").then((xlsx) => {
-      const data = getTableData();
-      const worksheet = xlsx.utils.json_to_sheet(data);
+      const worksheet = xlsx.utils.aoa_to_sheet(items);
       const workbook = { Sheets: { data: worksheet }, SheetNames: ["data"] };
       const excelBuffer = xlsx.write(workbook, {
         bookType: "xlsx",
