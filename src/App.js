6c0fcfeae39414679389d459b40d088689da8211
Vaghela Sagar <sagar19vaghela.sv@gmail.com>
2023-02-02 14:46:10 +0530

    download xlsx file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { items } from "./response";
+import FileSaver from "file-saver";
 
 function App() {
+  const getTableData = () => {
+    const excelData = [];
+    items.forEach((data, i) => {
+      const obj = {};
+      if (i > 0) {
+        items[0].forEach((item, i) => {
+          obj[item] = data[i];
+        });
+        if (Object.keys(obj).length !== 0) {
+          excelData.push(obj);
+        }
+      }
+    });
+    return excelData;
+  };
+
+  const saveAsExcelFile = (buffer, fileName, extension) => {
+    let EXCEL_TYPE =
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+    const data = new Blob([buffer], {
+      type: EXCEL_TYPE,
+    });
+    FileSaver.saveAs(data, fileName + "." + extension);
+  };
+
+  const handleClick = () => {
+    import("xlsx").then((xlsx) => {
+      const data = getTableData();
+      const worksheet = xlsx.utils.json_to_sheet(data);
+      const workbook = { Sheets: { data: worksheet }, SheetNames: ["data"] };
+      const excelBuffer = xlsx.write(workbook, {
+        bookType: "xlsx",
+        type: "array",
+      });
+      saveAsExcelFile(excelBuffer, "testfile", "xlsx");
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => {
+          handleClick();
+        }}
+      >
+        download file
+      </button>
     </div>
   );
 }
